9455d6332ef8bd076c1f3c228f1ff2da59fb09ab
Samyak S Sarnayak <samyak201@gmail.com>
2020-06-27 18:03:20 +0530

    [MAJOR] Digital signing, encryption, better POW and decentralisation
     - Digital Signatures: Each transaction has to be signed by the sender.
       The "sender" and "receiver" now represent public keys rather than
       random strings. Transactions are verified before adding.
     - Better proof of work algorithm: Includes list of transactions in the
       string which is hashed.
     - Encryption support (unused): added functions for public key
       encryption, not tested or used.
     - Better decentralisation: blocks are published as they are added.
       Transactions are not destroyed when replacing a chain, instead they
       are moved to the unconfirmed transactions. The source node is also
       added for each block.

--- blockchain.py
+++ blockchain.py
@@ -1,21 +1,72 @@
+import sys
 import hashlib
 import json
 from time import time
 from urllib.parse import urlparse
-from uuid import uuid4
+from typing import List
+import copy
 
+import nacl
 import requests
 from flask import Flask, jsonify, request
+from flask.json import JSONEncoder, JSONDecoder
 
+import encryption
+
+class Transaction:
+    """Wrapper class to represent a transaction
+
+    Arguments:
+    sender (bytes): sender public key
+    receiver (bytes): receiver public key
+    message (str): message to be sent
+    """
+    def __init__(self, sender: bytes, receiver: bytes, message: str, signature: str = None):
+        self.sender = sender
+        self.receiver = receiver
+        self.message = message
+        self.signature = signature
+        if signature is not None:
+            self.signature = bytes.fromhex(signature)
+
+    def __repr__(self):
+        return f"<{self.sender} {self.receiver} {self.message}>"
+
+    def to_dict(self):
+        return {
+            "sender": self.sender,
+            "receiver": self.receiver,
+            "message": self.message,
+            "signature": self.signature.hex()
+        }
+
+    def verify_transaction(self):
+        """Returns True if the signature is valid for this Transaction, returns False otherwise.
+
+        The message which the signature represents should be of the following form:
+        <sender_public_key receiver_public_key message>
+        """
+        if self.signature is None:
+            return False
+        try:
+            sender_key = encryption.decode_verify_key(self.sender)
+            encryption.decode_verify_key(self.receiver)
+        except (ValueError, TypeError) as e:
+            print("Error while verifying transaction:", e)
+            return False
+        return encryption.verify_message(sender_key,
+                                         bytes(repr(self), 'utf-8'),
+                                         self.signature)
 
 class Blockchain:
+    """Represents the entire blockchain present in a node"""
     def __init__(self):
         self.current_transactions = []
         self.chain = []
         self.nodes = set()
 
         # Create the genesis block
-        self.new_block(previous_hash='1', proof=100)
+        self.new_block(previous_hash='1', proof=100, transactions=[], verify=False)
 
     def register_node(self, address):
         """
@@ -53,15 +104,41 @@ class Blockchain:
             # Check that the hash of the block is correct
             last_block_hash = self.hash(last_block)
             if block['previous_hash'] != last_block_hash:
+                print(f"Invalid block hash at index {current_index}", file=sys.stderr)
                 return False
 
             # Check that the Proof of Work is correct
-            if not self.valid_proof(last_block['proof'], block['proof'], last_block_hash):
+            if not self.valid_proof(last_block['proof'], last_block_hash, block['proof'],
+                                    block["transactions"]):
+                print(f"Invalid proof of work at index {current_index}", file=sys.stderr)
                 return False
+            for transaction in block["transactions"]:
+                if not transaction.verify_transaction():
+                    print(f"Invalid transaction at index {current_index}", file=sys.stderr)
+                    return False
 
             last_block = block
             current_index += 1
 
+        return True
+
+    def replace_chain(self, chain: List):
+        """Replaces the blockchain with the longer (valid) "chain"
+        Note that validity isn't checked here
+
+        Arguments:
+        chain: chain to replace with
+        """
+        assert len(self.chain) < len(chain)
+        leftout_txs = []
+        min_len = len(self.chain)
+        i = 1
+        while i < min_len:
+            if self.hash(self.chain[i]) != self.hash(chain[i]):
+                leftout_txs.extend(self.chain[i]["transactions"])
+            i += 1
+        self.current_transactions.extend(leftout_txs)
+        self.chain = chain
         return True
 
     def resolve_conflicts(self):
@@ -80,25 +157,31 @@ class Blockchain:
 
         # Grab and verify the chains from all the nodes in our network
         for node in neighbours:
-            response = requests.get(f'http://{node}/chain')
+            response = requests.get(f'http://{node}/chain_length')
 
             if response.status_code == 200:
                 length = response.json()['length']
-                chain = response.json()['chain']
-
-                # Check if the length is longer and the chain is valid
-                if length > max_length and self.valid_chain(chain):
-                    max_length = length
-                    new_chain = chain
+                # check if length is longer
+                if length > max_length:
+                    chain_response = requests.get(f'http://{node}/chain')
+                    if chain_response.status_code == 200:
+                        chain = chain_response.json(cls=BlockchatJSONDecoder)['chain']
+                        # Check if the chain is valid
+                        print(chain)
+                        if self.valid_chain(chain):
+                            max_length = length
+                            new_chain = chain
+                        else:
+                            print(f"Invalid chain on node {node}")
 
         # Replace our chain if we discovered a new, valid chain longer than ours
         if new_chain:
-            self.chain = new_chain
+            self.replace_chain(new_chain)
             return True
 
         return False
 
-    def new_block(self, proof, previous_hash):
+    def new_block(self, proof, previous_hash, transactions, verify=True):
         """
         Create a new Block in the Blockchain
 
@@ -110,18 +193,61 @@ class Blockchain:
         block = {
             'index': len(self.chain) + 1,
             'timestamp': time(),
-            'transactions': self.current_transactions,
+            'transactions': transactions,
             'proof': proof,
             'previous_hash': previous_hash or self.hash(self.chain[-1]),
+            'source_node': node_url
         }
 
         # Reset the current list of transactions
         self.current_transactions = []
 
-        self.chain.append(block)
+        if verify:
+            self.add_block(block)
+            self.publish_block(block)
+        else:
+            self.chain.append(block)
         return block
 
-    def new_transaction(self, sender, recipient, amount):
+    def publish_block(self, block):
+        """Publishes block to all nodes"""
+        headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+        for node in self.nodes:
+            if node != block["source_node"]:
+                response = requests.post(f'http://{node}/add_block',
+                                         data=json.dumps({"block": block},
+                                                         cls=BlockchatJSONEncoder),
+                                         headers=headers)
+                if not response.ok:
+                    print(f"Could not publish block to node {node}", file=sys.stderr)
+
+
+    def add_block(self, block):
+        """
+        Add an already mined block to the chain
+
+        :param block: The block to be added
+        """
+        last_block = self.last_block
+        last_hash = self.hash(last_block)
+        last_proof = last_block["proof"]
+
+        # check if previous hash matches last block on our chain
+        if last_hash != block["previous_hash"]:
+            return False
+        # check if proof is correct
+        for transaction in block["transactions"]:
+            if not transaction.verify_transaction():
+                return False
+        if not self.valid_proof(last_proof, last_hash, block["proof"], block["transactions"]):
+            return False
+
+        self.chain.append(block)
+        return True
+
+    def new_transaction(self, sender: bytes, recipient: bytes, message: str,
+                        signature: bytes = None, self_sign: bool = False,
+                        add_to: List = None):
         """
         Creates a new transaction to go into the next mined Block
 
@@ -130,11 +256,17 @@ class Blockchain:
         :param amount: Amount
         :return: The index of the Block that will hold this transaction
         """
-        self.current_transactions.append({
-            'sender': sender,
-            'recipient': recipient,
-            'amount': amount,
-        })
+        transaction = Transaction(sender, recipient, message, signature)
+        print(transaction)
+        if signature is None and self_sign is True:
+            message, signature = encryption.sign_message(node_secret, repr(transaction))
+            transaction.signature = signature
+        if not transaction.verify_transaction():
+            return False
+
+        if add_to is None:
+            add_to = self.current_transactions
+        add_to.append(transaction)
 
         return self.last_block['index'] + 1
 
@@ -151,16 +283,16 @@ class Blockchain:
         """
 
         # We must make sure that the Dictionary is Ordered, or we'll have inconsistent hashes
-        block_string = json.dumps(block, sort_keys=True).encode()
+        block_string = json.dumps(block, sort_keys=True, cls=BlockchatJSONEncoder).encode()
         return hashlib.sha256(block_string).hexdigest()
 
-    def proof_of_work(self, last_block):
+    def proof_of_work(self, last_block, transactions):
         """
         Simple Proof of Work Algorithm:
 
          - Find a number p' such that hash(pp') contains leading 4 zeroes
          - Where p is the previous proof, and p' is the new proof
-         
+
         :param last_block: <dict> last Block
         :return: <int>
         """
@@ -169,13 +301,13 @@ class Blockchain:
         last_hash = self.hash(last_block)
 
         proof = 0
-        while self.valid_proof(last_proof, proof, last_hash) is False:
+        while self.valid_proof(last_proof, last_hash, proof, transactions) is False:
             proof += 1
 
         return proof
 
     @staticmethod
-    def valid_proof(last_proof, proof, last_hash):
+    def valid_proof(last_proof, last_hash, proof, transactions: List[Transaction]):
         """
         Validates the Proof
 
@@ -186,16 +318,53 @@ class Blockchain:
 
         """
 
-        guess = f'{last_proof}{proof}{last_hash}'.encode()
+        transactions_representation = "".join(map(repr, transactions))
+        guess = f'{last_proof}{proof}{last_hash}{transactions_representation}'.encode()
         guess_hash = hashlib.sha256(guess).hexdigest()
-        return guess_hash[:4] == "0000"
+        ret = guess_hash[:4] == "0000"
+        if ret:
+            print("Guess:", guess)
+        return ret
 
 
 # Instantiate the Node
 app = Flask(__name__)
 
+# custom JSONEncoder for our custom classes
+class BlockchatJSONEncoder(JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, Transaction):
+            return obj.to_dict()
+        return super(BlockchatJSONEncoder, self).default(obj)
+class BlockchatJSONDecoder(JSONDecoder):
+    def __init__(self, *args, **kwargs):
+        self.orig_obj_hook = kwargs.pop("object_hook", None)
+        super(BlockchatJSONDecoder, self).__init__(
+            *args,
+            object_hook=self.custom_obj_hook, **kwargs)
+
+    def custom_obj_hook(self, dct):
+        # Calling custom decode function:
+        if "sender" in dct and "receiver" in dct and "sender" in dct and "signature" in dct:
+            dct = Transaction(dct["sender"], dct["receiver"], dct["message"], dct["signature"])
+        if self.orig_obj_hook:  # Do we have another hook to call?
+            return self.orig_obj_hook(dct)  # Yes: then do it
+        return dct  # No: just return the decoded dict
+app.json_encoder = BlockchatJSONEncoder
+app.json_decoder = BlockchatJSONDecoder
+
 # Generate a globally unique address for this node
-node_identifier = str(uuid4()).replace('-', '')
+try:
+    with open("NODE_KEY.txt", "rb") as f:
+        node_secret = nacl.signing.SigningKey(f.read())
+except Exception as e:
+    print("Could not load secret key. Error:", e)
+    node_identifier, node_secret = encryption.generate_signing_key()
+    with open("NODE_KEY.txt", "wb") as f:
+        f.write(node_secret.encode())
+with open("NODE_ADDR.txt", "rt") as f:
+    node_url = f.read().strip()
+node_identifier = encryption.encode_verify_key(node_secret.verify_key).decode()
 
 # Instantiate the Blockchain
 blockchain = Blockchain()
@@ -203,21 +372,22 @@ blockchain = Blockchain()
 
 @app.route('/mine', methods=['GET'])
 def mine():
-    # We run the proof of work algorithm to get the next proof...
-    last_block = blockchain.last_block
-    proof = blockchain.proof_of_work(last_block)
+    # ensure chain is the best before mining
+    blockchain.resolve_conflicts()
 
-    # We must receive a reward for finding the proof.
-    # The sender is "0" to signify that this node has mined a new coin.
-    blockchain.new_transaction(
-        sender="0",
-        recipient=node_identifier,
-        amount=1,
-    )
+    last_block = blockchain.last_block
+    # get the transactions to be added
+    transactions = copy.deepcopy(blockchain.current_transactions)
+    # add a "mine" transaction
+    blockchain.new_transaction(node_identifier, node_identifier, "<<MINE>>",
+                               self_sign=True, add_to=transactions)
+
+    # We run the proof of work algorithm to get the next proof...
+    proof = blockchain.proof_of_work(last_block, transactions)
 
     # Forge the new Block by adding it to the chain
     previous_hash = blockchain.hash(last_block)
-    block = blockchain.new_block(proof, previous_hash)
+    block = blockchain.new_block(proof, previous_hash, transactions)
 
     response = {
         'message': "New Block Forged",
@@ -234,12 +404,15 @@ def new_transaction():
     values = request.get_json()
 
     # Check that the required fields are in the POST'ed data
-    required = ['sender', 'recipient', 'amount']
+    required = ['sender', 'recipient', 'message', 'signature']
     if not all(k in values for k in required):
         return 'Missing values', 400
 
     # Create a new Transaction
-    index = blockchain.new_transaction(values['sender'], values['recipient'], values['amount'])
+    index = blockchain.new_transaction(values['sender'], values['recipient'],
+                                       values['message'], values['signature'])
+    if not index:
+        return "Cannot verify transaction", 400
 
     response = {'message': f'Transaction will be added to Block {index}'}
     return jsonify(response), 201
@@ -253,6 +426,25 @@ def full_chain():
     }
     return jsonify(response), 200
 
+@app.route('/chain_length', methods=['GET'])
+def chain_length():
+    response = {
+        'length': len(blockchain.chain),
+    }
+    return jsonify(response), 200
+
+@app.route('/add_block', methods=['POST'])
+def add_block():
+    values = request.get_json()
+    block_to_add = values.get('block')
+
+    # try to add block
+    success = blockchain.add_block(block_to_add)
+
+    if success:
+        return jsonify({
+            "message": "Block added successfully"}), 200
+    return "Error: Invalid block", 400
 
 @app.route('/nodes/register', methods=['POST'])
 def register_nodes():
@@ -265,6 +457,8 @@ def register_nodes():
     for node in nodes:
         blockchain.register_node(node)
 
+    blockchain.resolve_conflicts()
+
     response = {
         'message': 'New nodes have been added',
         'total_nodes': list(blockchain.nodes),
@@ -275,6 +469,7 @@ def register_nodes():
 @app.route('/nodes/resolve', methods=['GET'])
 def consensus():
     replaced = blockchain.resolve_conflicts()
+    print("Replaced:", replaced)
 
     if replaced:
         response = {
@@ -298,4 +493,4 @@ if __name__ == '__main__':
     args = parser.parse_args()
     port = args.port
 
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, threaded=False, processes=1)
---
+++ encryption.py
@@ -0,0 +1,75 @@
+import nacl.encoding
+import nacl.signing
+import nacl.exceptions
+import nacl.utils
+import nacl.public
+
+### Digital Signatures ###
+
+def generate_signing_key() -> (nacl.signing.VerifyKey, nacl.signing.SigningKey):
+    """Generate a new random signing key
+
+    Returns:
+    verify_key: key for verification, can be given out
+    signing_key: secret signing key, do not give out
+    """
+    signing_key = nacl.signing.SigningKey.generate()
+
+    verify_key = signing_key.verify_key
+
+    return verify_key, signing_key
+
+def encode_verify_key(verify_key: nacl.signing.VerifyKey) -> bytes:
+    """Encodes given verify_key into a bytes object"""
+    return verify_key.encode(encoder=nacl.encoding.HexEncoder)
+
+def decode_verify_key(verify_key_hex: bytes) -> nacl.signing.VerifyKey:
+    """Decodes given verify_key_hex into a verify_key"""
+    return nacl.signing.VerifyKey(verify_key_hex, encoder=nacl.encoding.HexEncoder)
+
+def sign_message(signing_key: nacl.signing.SigningKey, message):
+    """Signs message using signing_key"""
+    if isinstance(message, str):
+        message = message.encode()
+    signed = signing_key.sign(message)
+    return signed.message, signed.signature
+
+def verify_message(verify_key: nacl.signing.VerifyKey, message: bytes, signature: bytes):
+    """Verifies that message has been signed by signature using the verify_key"""
+    try:
+        verify_key.verify(message, signature)
+        return True
+    except nacl.exceptions.BadSignatureError:
+        return False
+
+### END ###
+
+### Public Key Encryption ###
+
+def generate_keypair() -> (nacl.public.PublicKey, nacl.public.PrivateKey):
+    """Generates a new random public-private key pair
+    Returns:
+    pk: public key
+    sk: secret/private key
+    """
+    sk = nacl.public.PrivateKey.generate()
+    pk = sk.public_key
+    return pk, sk
+
+def generate_box(one_sk: nacl.public.PrivateKey,
+                 another_pk: nacl.public.PublicKey) -> nacl.public.Box:
+    """Generates a secret box using one user's private key and another's public key"""
+    return nacl.public.Box(one_sk, another_pk)
+
+def encrypt_message(box: nacl.public.Box, message) -> bytes:
+    """Encrypts message using box"""
+    if isinstance(message, str):
+        message = message.encode()
+    return box.encrypt(message)
+
+def decrypt_message(box: nacl.public.Box, encrypted: bytes) -> str:
+    """Decryptes encrypted message using Box"""
+    decrypted = box.decrypt(encrypted)
+    return decrypted.decode('utf-8')
+
+### END ###
